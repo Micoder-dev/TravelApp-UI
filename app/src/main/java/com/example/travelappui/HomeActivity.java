31bc07befb95c30a26225ec28dc8804fe77e3165
Micoder-dev <micoder.com@gmail.com>
2022-08-30 21:20:12 +0530

    simple 'Floating Navigation View' implemented

--- app/src/main/java/com/example/travelappui/HomeActivity.java
+++ app/src/main/java/com/example/travelappui/HomeActivity.java
@@ -6,24 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.andremion.floatingnavigationview.FloatingNavigationView;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.shape.CornerFamily;
 import com.google.android.material.shape.MaterialShapeDrawable;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
     private MaterialToolbar mToolBar;
+
+    private FloatingNavigationView mFloatingNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Custom Action Bar
         mToolBar = (MaterialToolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolBar);
-
         float radius = getResources().getDimension(com.shashank.sony.fancywalkthroughlib.R.dimen.activity_half_margin);
         MaterialShapeDrawable toolbarBackground = (MaterialShapeDrawable) mToolBar.getBackground();
         toolbarBackground.setShapeAppearanceModel(
@@ -33,13 +39,38 @@ public class HomeActivity extends AppCompatActivity {
                         .setBottomLeftCorner(CornerFamily.ROUNDED,radius)
                         .build()
         );
-
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
-
         LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View actionBarView = layoutInflater.inflate(R.layout.custom_app_bar, null);
         actionBar.setCustomView(actionBarView);
 
+        // Floating Navigation View
+        mFloatingNavigationView = (FloatingNavigationView) findViewById(R.id.floating_navigation_view);
+        mFloatingNavigationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFloatingNavigationView.open();
+            }
+        });
+        mFloatingNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Snackbar.make((View) mFloatingNavigationView.getParent(), item.getTitle() + " Selected!", Snackbar.LENGTH_SHORT).show();
+                mFloatingNavigationView.close();
+                return true;
+            }
+        });
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mFloatingNavigationView.isOpened()) {
+            mFloatingNavigationView.close();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
